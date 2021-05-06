c99d4b66ad7df61c37f97e94968deb9879a92381
jeeyunryu <jeeyunryu@gmail.com>
2021-05-06 11:01:10 +0900

    Update AllConverter.java

--- app/src/main/java/edu/handong/csee/java/hw2/converters/AllConverter.java
+++ app/src/main/java/edu/handong/csee/java/hw2/converters/AllConverter.java
@@ -1,46 +1,69 @@
-package edu.handong.csee.java.hw2.converters;
-//
-//myAllConverter.setFromValue(fromValue).setOriginalMeasure(originalMeasure).convertAndPrintOut();
-public class AllConverter {
+package edu.handong.csee.java.hw2;
 
-    private double fromValue;
-    private String originalMeasure;
-    private double kilometers;
-    private double meters;
-    private double miles;
-    private double tons;
-    private double grams;
-    private double kilograms;
-    
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import edu.handong.csee.java.hw2.converters.*;
+
+/**
+ * Perform and print possible unit conversions.
+ */
+public class AllConverter extends IntegratedConverter{
+    private double value;
+    private String name;
+    private String [][]target = {
+        {"KM","TON"},
+        {"M","KG"},
+        {"MILE","G"}
+    };
+    /**
+     * set the value
+     */
     public AllConverter setFromValue(double fromValue){
-        this.fromValue = fromValue;
+        this.value = fromValue;
         return this;
-
     }
-
-    public AllConverter setOriginalMeasure(String originalMeasure){
-        this.originalMeasure = originalMeasure;
-        return this;
-
+    /**
+     * set the name value
+     */
+    public AllConverter setOriginalMeasure(String origin){
+        this.name = origin;
+        return this; 
     }
-
-
-     public void convertAndPrintout(){
-        if(originalMeasure.equals("KM")){
-
-            meters = kilometers * 1000;
-            miles = kilometers / 1.6;
-            System.out.println(this.fromValue + " " + this.originalMeasure + " is " + meters + " " + "M" + "!");
-            System.out.println(this.fromValue + " " + this.originalMeasure + " is " + miles + " " + "MILE" + "!");
-        }
-        if(originalMeasure.equals("TON")){
-            grams = tons * 907185;
-            kilograms = tons * 907.185;
-
-            System.out.println(this.fromValue + " " + this.originalMeasure + " is " + grams + " " + "G" + "!");
-            System.out.println(this.fromValue + " " + this.originalMeasure + " is " + kilograms + " " + "KG" + "!");
-        }
-      
-    }
+    /**
+     * Perform and print possible unit conversions.
+     */
+    public void convertAndPrintOut(){
+        if(name.equals(target[0][0])){
+            for(int i = 1; i < 3; i++){
+                String convertName = "edu.handong.csee.java.hw2.converters." + name + "To" + target[i][0] + "Converter";
+                try {
+                    Class<?> converterClass = (Class<?>) Class.forName(convertName);
+                    Constructor<?> constructor = (Constructor<?>) converterClass.getConstructor();
+                    Convertible myConverter = (Convertible) constructor.newInstance();
+                    myConverter.setFromValue(value);
+                    myConverter.convert();
+                    System.out.println(value +" " + name + " is " + myConverter.getConvertedValue() + " " + target[i][0]);
+                } catch(ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                    System.out.println("The converter (" + convertName + ") for " + name + " to " + target[i][0] + " does not exist!!");
+                }
+            }
+        }else if(name.equals(target[0][1])){
+            for(int i = 1; i < 3; i++){
+                String convertName = "edu.handong.csee.java.hw2.converters." + name + "To" + target[i][1] + "Converter";
+                try {
+                    Class<?> converterClass = (Class<?>) Class.forName(convertName);
+                    Constructor<?> constructor = (Constructor<?>) converterClass.getConstructor();
+                    Convertible myConverter = (Convertible) constructor.newInstance();
+                    myConverter.setFromValue(value);
+                    myConverter.convert();
+                    System.out.println(value +" " + name + " is " + myConverter.getConvertedValue() + " " + target[i][1]);
     
+                } catch(ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                    System.out.println("The converter (" + convertName + ") for " + name + " to " + target[i][1] + " does not exist!!");
+                }
+            }
+        }else{
+            System.out.println("AllConverter cannot support the measure!");
+            }
+        }
 }
